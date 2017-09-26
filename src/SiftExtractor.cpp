37fab7184f8e6b5e78d5f0c25604ebbfd1010cde
gaoqian506 <gaoqian506@163.com>
2017-09-26 23:27:57 +0800

    Implementing SiftExtractor::extract

--- src/SiftExtractor.cpp
+++ src/SiftExtractor.cpp
@@ -8,6 +8,13 @@ namespace ww {
 
 void SiftExtractor::extract() {
 
+	//for each image {
+
+		// img_rsz = resize(img);
+		// img_ppr = preprocess(img_rsz);
+		// features = detect(img_ppr);
+		// write(img_ppr, features);
+	//}
 
 }
 
